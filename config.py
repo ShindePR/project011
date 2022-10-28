33180d9e9b815ee145d28998ad6b256ed3836a2a
ShindePR <ps915350@gmail.com>
2022-10-28 13:30:15 +0530

    Added all files

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+CSV_FILE_PATH = "enter_path"
+
+PORT_NUMBER = 5000
+
+MODEL_FILE_PATH = os.path.join("models","Medical_model.pkl")
+
+JSON_FILE_PATH = os.path.join("models","data.json")
+
+
